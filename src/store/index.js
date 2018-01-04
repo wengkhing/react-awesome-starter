adb31a28dc46e9be1767b89a18769e15ed918a8f
wengkhing <wengkhing@gmail.com>
2018-01-04 17:25:22 +0800

    feat: make standard support flow

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
-/* @flow */
-
 import { routerMiddleware } from 'react-router-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import axios from 'axios'
 import rootReducer from './reducers'
 
-export default (history, initialState = {}) => {
+export default (history: Object, initialState: Object = {}) => {
   const enhancers = []
   const middleware = [
     thunk.withExtraArgument(axios),
