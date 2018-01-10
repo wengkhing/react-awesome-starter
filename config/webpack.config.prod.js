21f0c30d83baaa7f804599a97ea8a58845f4ed48
Kok Eng <kok.eng@dd.express>
2018-01-10 17:27:12 +0800

    fix(css): disable css-module

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -181,7 +181,7 @@ module.exports = {
                       loader: require.resolve('css-loader'),
                       options: {
                         importLoaders: 1,
-                        modules: true,
+                        // modules: true,
                         minimize: true,
                         sourceMap: shouldUseSourceMap
                       }
