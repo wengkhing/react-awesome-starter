28a2538cbbcc6c97bd8c6185fef4ee2d95102b8f
wengkhing <wengkhing@gmail.com>
2018-01-12 11:05:54 +0800

    patch: reduce helmet code size

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <Helmet>
-          <meta charSet='utf-8' />
-          <title>App Title</title>
-        </Helmet>
+        <Helmet title='App Title' />
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
