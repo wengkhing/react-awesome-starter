a793c2a21cbe5c3f375e25495f6803f8d4577f43
wengkhing <wengkhing@gmail.com>
2018-01-19 10:51:52 +0800

    feat: uneject create-react-app and integrate custom-react-script

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Helmet } from 'react-helmet'
 import logo from './logo.svg'
-import './App.css'
+import './App.scss'
 
 class App extends Component {
   render () {
