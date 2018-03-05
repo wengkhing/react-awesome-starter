f910dfdbdf32033bc150d811fa9268c868bc24f1
wengkhing <wengkhing@gmail.com>
2018-03-06 01:56:04 +0800

    add interruptive loader animation

--- src/framework/InterruptiveLoader/InterruptiveLoader.js
+++ src/framework/InterruptiveLoader/InterruptiveLoader.js
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+import { Transition } from 'react-transition-group'
 
 import './InterruptiveLoader.scss'
+
+const loaderTransition = {
+  exited: {
+    padding: '120px 60px 90px 60px',
+    opacity: 0
+  },
+  exiting: {
+    padding: '120px 60px 90px 60px',
+    opacity: 0
+  },
+  entering: {
+    padding: '120px 60px 90px 60px',
+    opacity: 0
+  },
+  entered: {
+    padding: '60px 60px 30px 60px',
+    opacity: 1
+  }
+}
+
+const overlayTransition = {
+  exited: { opacity: 0 },
+  exiting: { opacity: 0 },
+  entering: { opacity: 0 },
+  entered: { opacity: 1 }
+}
 
 export class InterruptiveLoader extends Component {
   componentDidUpdate () {
@@ -9,17 +36,24 @@ export class InterruptiveLoader extends Component {
 
   renderLoader () {
     const { isLoading, message } = this.props
-    if (isLoading) {
-      return (
-        <div className='loader-wrapper'>
-          <div className='overlay' />
-          <div className='loader'>
-            <div className='spinner' />
-            <p>{message || 'Spinning up the hamster'}</p>
+    return (
+      <Transition in={isLoading}
+        timeout={{ enter: 0, exit: 400 }}
+        appear
+        unmountOnExit>
+        {(state) => (
+          <div className='loader-wrapper'>
+            <div style={{ ...overlayTransition[state] }}
+              className='overlay' />
+            <div style={{ ...loaderTransition[state] }}
+              className='loader'>
+              <div className='spinner' />
+              <p>{message || 'Spinning up the hamster'}</p>
+            </div>
           </div>
-        </div>
-      )
-    }
+        )}
+      </Transition>
+    )
   }
 
   render () {
