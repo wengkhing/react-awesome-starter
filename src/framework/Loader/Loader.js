dee6a90f632dba6822c1804cd061aec64bddb45f
wengkhing <wengkhing@gmail.com>
2018-03-05 22:51:42 +0800

    fix breaking loader when isLoading props is changing fast

--- src/framework/Loader/Loader.js
+++ src/framework/Loader/Loader.js
@@ -1,38 +1,86 @@
 import React, { Component } from 'react'
 import ProgressBar from 'progressbar.js'
+import _ from 'lodash'
 
 import './Loader.scss'
 
 export class Loader extends Component {
-  componentDidUpdate () {
+  constructor (props) {
+    super(props)
+    this.uniqueId = _.random(0, 100)
+    this.state = {
+      visible: false,
+      mount: false
+    }
+
+    this.hideLoader = this.hideLoader.bind(this)
+    this.showLoader = this.showLoader.bind(this)
+  }
+
+  componentDidMount () {
     const {
-      duration = 8000,
       color = '#4567cd',
-      isLoading = false,
       strokeWidth = 1,
       trailColor = 'rgba(130, 130, 130, 0.5)'
     } = this.props
 
+    this.loader = new ProgressBar.Line(
+      `.loader-${this.uniqueId}`,
+      { color, trailColor, strokeWidth }
+    )
+
+    this.loader.set(1)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    const {
+      duration = 8000,
+      isLoading = false
+    } = nextProps
+
+    console.log(this.timeout)
+
+    if (this.timeout) clearTimeout(this.timeout)
+
     if (isLoading) {
-      this.loader = new ProgressBar.Line(
-        '.component-loader',
-        { color, trailColor, strokeWidth })
+      this.showLoader()
       this.loader.animate(0.9, { duration })
     } else {
-      if (this.loader) {
-        setTimeout(() => {
-          this.loader.set(1)
-          setTimeout(() => {
-            if (this.loader) this.loader.destroy()
-          }, 900)
-        }, 500)
-      }
+      this.loader.animate(1, {
+        duration: 300,
+        easing: 'easeInOut'
+      }, () => {
+        this.hideLoader()
+      })
     }
+  }
+
+  hideLoader () {
+    this.setState({
+      visible: false
+    })
+
+    this.timeout = setTimeout(() => {
+      this.setState({
+        mount: false
+      })
+    }, 300)
+  }
+
+  showLoader () {
+    this.loader.set(0)
+    this.setState({
+      mount: true,
+      visible: true
+    })
   }
 
   render () {
     return (
-      <div className='component-loader' />
+      <div className={`component-loader
+        loader-${this.uniqueId}
+        ${this.state.visible ? '' : '--transparent'}
+        ${this.state.mount ? '' : '--unmount'}`} />
     )
   }
 }
